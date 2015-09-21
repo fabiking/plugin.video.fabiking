1d1e09f6fde3be5ea5b35c49c6c671659cf719c6
fabiking <fabian.rincon.vi@gmail.com>
2015-09-21 14:06:41 +0200

    ver. 1.1

--- default.py
+++ default.py
@@ -5692,7 +5692,7 @@ def xml_skin():
                 mastermenu = plugintools.get_setting("mastermenu")
                 if mastermenu == "":
                     plugintools.log("[PalcoTV.xml_skin: No definido")
-                    mastermenu = 'http://pastebin.com/raw.php?i=RBEDZWqN'
+                    mastermenu = 'https://dl.dropboxusercontent.com/u/8036850/PalcoTV/PalcoTV-0.3.2.xml'
                     
                     # Control para ver la intro de PalcoTV
                     ver_intro = plugintools.get_setting("ver_intro")
